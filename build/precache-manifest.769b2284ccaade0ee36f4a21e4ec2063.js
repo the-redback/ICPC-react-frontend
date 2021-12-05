27f98f4e56ff4c9bcfdd6e100fa8d0ece0c8b862
the-redback <maruf.2hin@gmail.com>
2021-12-04 21:23:13 -0600

    Everything of npm build

---
+++ build/precache-manifest.769b2284ccaade0ee36f4a21e4ec2063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8256152d6fc0b7b175762fd502668a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "134bb8417c619e506677",
+    "url": "/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "3c6e7e4ea3565ead4b77",
+    "url": "/static/css/main.3c39e833.chunk.css"
+  },
+  {
+    "revision": "134bb8417c619e506677",
+    "url": "/static/js/2.56ada87c.chunk.js"
+  },
+  {
+    "revision": "3c6e7e4ea3565ead4b77",
+    "url": "/static/js/main.f29a8522.chunk.js"
+  },
+  {
+    "revision": "d80f0bc4a8caa3c0c4de",
+    "url": "/static/js/runtime-main.2cefdcfc.js"
+  }
+]);
